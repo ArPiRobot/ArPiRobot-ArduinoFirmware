c925bf1c4a358f6ec1de2f7287918c187f5592d6
Marcus Behel <marcus.behel@mindspring.com>
2020-10-10 21:18:36 -0400

    Fix arduino nano every analogInputToDigitalPin

--- ArPiRobot-ArduinoFirmware/support.h
+++ ArPiRobot-ArduinoFirmware/support.h
@@ -4,24 +4,23 @@
 
 // For some reason this is not defined for the arduino nano every...
 uint8_t analogInputToDigitalPin(uint8_t p){
-  // Inverse mapping of analog_pin_to_channel array in pins_arduino.h for megaAVR 4809 
   switch(p){
-  case 3:
-    return 0;
-  case 2:
-    return 1;
-  case 1:
-    return 2;
   case 0:
-    return 3;
-  case 12:
-    return 4;
-  case 13:
-    return 5;
+    return A0;
+  case 1:
+    return A1;
+  case 2:
+    return A2;
+  case 3:
+    return A3;
   case 4:
-    return 6;
+    return A4;
   case 5:
-    return 7;
+    return A5;
+  case 6:
+    return A6;
+  case 7:
+    return A7;
   }
 }
 
